9c540c7a7b5ec0f430a402bf2036b0253fd5a931
Ashutosh singh <55102089+Ashutosh619-sudo@users.noreply.github.com>
2019-11-25 20:50:46 +0530

    displays a text of number of people currently.

--- FaceCounter.py
+++ FaceCounter.py
@@ -13,8 +13,11 @@ while True:
     rbg_frame = frame[:, :, ::-1]
     face_location = face_recognition.face_locations(rbg_frame)
     faces = len(face_location)
+    people = "People: "+str(faces)
     for top,right,bottom,left in face_location:
         cv2.rectangle(frame, (left, top), (right,bottom),(0,255,0),2)
+        
+    cv2.putText(frame,people,(20,50),cv2.FONT_HERSHEY_SIMPLEX,1,(255,0,0),3)   
 
     cv2.imshow("face_recongition", frame)
 
